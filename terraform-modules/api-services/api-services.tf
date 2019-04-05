9f3f5816574a77f16b1d2982891813fadca40943
Mark Sarcevicz <smark@broadinstitute.org>
2019-04-05 15:50:39 -0400

    fixed provider

--- terraform-modules/api-services/api-services.tf
+++ terraform-modules/api-services/api-services.tf
@@ -1,17 +1,17 @@
 # ServiceUsage API
 resource "google_project_service" "serviceusage" {
-  provider = "${var.provider}"
+  provider = "google"
   project = "${var.project}"
   service = "serviceusage.googleapis.com"
 }
 # cloudresourcemanager API
 resource "google_project_service" "cloudresourcemanager" {
-  provider = "${var.provider}"
+  provider = "google"
   project = "${var.project}"
   service = "cloudresourcemanager.googleapis.com"
 }
 resource "google_project_service" "required-services" {
-  provider = "${var.provider}"
+  provider = "google"
   project = "${var.project}"
   count = "${length(var.services)}"
   service = "${element(var.services, count.index)}"
