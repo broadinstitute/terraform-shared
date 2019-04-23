acffc37b56c59682a56483c54d9441460540b6bf
Henry Ferrara <hjferrara@bynara.com>
2019-04-23 11:12:56 -0400

    explicitly state provider so module users must provide it updated readme

--- terraform-modules/api-services/api-services.tf
+++ terraform-modules/api-services/api-services.tf
@@ -1,17 +1,17 @@
 # ServiceUsage API
 resource "google_project_service" "serviceusage" {
-  provider = "google"
+  provider = "google.target"
   project = "${var.project}"
   service = "serviceusage.googleapis.com"
 }
 # cloudresourcemanager API
 resource "google_project_service" "cloudresourcemanager" {
-  provider = "google"
+  provider = "google.target"
   project = "${var.project}"
   service = "cloudresourcemanager.googleapis.com"
 }
 resource "google_project_service" "required-services" {
-  provider = "google"
+  provider = "google.target"
   project = "${var.project}"
   count = "${length(var.services)}"
   service = "${element(var.services, count.index)}"
