8b460e4cf776b4bd2bb05dd4929efb9e0b0b9d5b
Raphael Luckom <rluckom@broadinstitute.org>
2019-07-30 15:08:27 -0400

    add instance names output

--- terraform-modules/docker-instance-data-disk/outputs.tf
+++ terraform-modules/docker-instance-data-disk/outputs.tf
@@ -7,6 +7,10 @@ output "instance_private_ips" {
   value = "${google_compute_instance.instance.*.network_interface.0.network_ip}"
 }
 
+output "instance_names" {
+  value = "${google_compute_instance.instance.*.name}"
+}
+
 output "instance_instance_group" {
   value = "${google_compute_instance_group.instance-group-unmanaged.*.self_link}"
 }
