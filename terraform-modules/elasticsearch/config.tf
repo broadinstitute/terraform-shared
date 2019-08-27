4a2f0468f35294955347a9aab31b885a3aeab54f
Raphael Luckom <rluckom@broadinstitute.org>
2019-08-27 13:30:03 -0400

    add clustering

--- terraform-modules/elasticsearch/config.tf
+++ terraform-modules/elasticsearch/config.tf
@@ -15,13 +15,15 @@ services:
       memlock: -1
       nofile: 65536
     environment:
-      - bootstrap.memory_lock=true
-      - network.bind_host=_eth0_
       - network.publish_host=${data.null_data_source.hostnames_with_no_trailing_dot[count.index].outputs.hostname_priv}
       - network.host=${data.null_data_source.hostnames_with_no_trailing_dot[count.index].outputs.hostname_priv}
+      - node.name=${module.instances.instance_names[count.index]}
+      - cluster.name=${var.owner}-${var.service}
+      - discovery.zen.ping.unicast.hosts=${jsonencode(module.instances.instance_names)}
+      - bootstrap.memory_lock=true
+      - network.bind_host=_eth0_
       - http.cors.allow-origin='*'
       - http.cors.enabled=true
-      - node.name=${module.instances.instance_names[count.index]}
       - xpack.security.enabled=false
       - xpack.graph.enabled=false
       - xpack.ml.enabled=false
@@ -33,7 +35,7 @@ services:
       - "9200:9200"
       - "9300:9300"
     volumes:
-      - ${var.application_data_path}:/usr/share/elasticsearch/data
+      - ${var.application_data_path}:/usr/share/elasticsearcih/data
 EOT
   bucket = "${google_storage_bucket.config-bucket.name}"
   depends_on = [ data.null_data_source.hostnames_with_no_trailing_dot, module.instances, google_storage_bucket.config-bucket]
