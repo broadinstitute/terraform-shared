4496e156d7ce3cd8ffd10aa468a24d9166cbfd07
Raphael Luckom <rluckom@broadinstitute.org>
2019-08-30 13:21:57 -0400

    fix typo

--- terraform-modules/elasticsearch/config.tf
+++ terraform-modules/elasticsearch/config.tf
@@ -15,7 +15,7 @@ services:
       memlock: -1
       nofile: 65536
     environment:
-    - network.publish_host=${length(data.null_data_source.hostnames_with_no_trailing_dot) > 0 ? data.null_data_source.hostnames_with_no_trailing_dot[count.index].outputs.hostname_priv : ""}
+      - network.publish_host=${length(data.null_data_source.hostnames_with_no_trailing_dot) > 0 ? data.null_data_source.hostnames_with_no_trailing_dot[count.index].outputs.hostname_priv : ""}
       - network.host=${length(data.null_data_source.hostnames_with_no_trailing_dot) > 0 ? data.null_data_source.hostnames_with_no_trailing_dot[count.index].outputs.hostname_priv : ""}
       - node.name=${module.instances.instance_names[count.index]}
       - cluster.name=${var.owner}-${var.service}
