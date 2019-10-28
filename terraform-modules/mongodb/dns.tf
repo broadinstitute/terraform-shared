2f21166e43d2c71aea25eab92c4b1f24eac3d65d
Greg Malkov <gmalkov@broadinstitute.org>
2019-10-28 15:37:25 -0400

    syntax

--- terraform-modules/mongodb/dns.tf
+++ terraform-modules/mongodb/dns.tf
@@ -11,8 +11,8 @@ data "google_dns_managed_zone" "dns-zone" {
 resource "google_dns_record_set" "dns-a" {
   provider     = "google.dns"
   count        = var.dns_zone_name != "none" ? "${length(var.mongodb_roles)}" : 0
-  managed_zone = "${data.google_dns_managed_zone.dns-zone.name}"
-  name         = "${format("${var.owner}-${var.service}-%02d.%s",count.index+1,data.google_dns_managed_zone.dns-zone.dns_name)}"
+  managed_zone = "${var.dns_zone_name != "none" ? data.google_dns_managed_zone.dns-zone.name : ""}"
+  name         = "${var.dns_zone_name != "none" ? format("${var.owner}-${var.service}-%02d.%s",count.index+1,data.google_dns_managed_zone.dns-zone.dns_name) : ""}"
   type         = "A"
   ttl          = "${var.dns_ttl}"
   rrdatas      = [ "${element(module.instances.instance_public_ips, count.index)}" ]
@@ -22,7 +22,7 @@ resource "google_dns_record_set" "dns-a" {
 resource "google_dns_record_set" "dns-a-priv" {
   provider     = "google.dns"
   count        = var.dns_zone_name != "none" ? "${length(var.mongodb_roles)}" : 0
-  managed_zone = "${data.google_dns_managed_zone.dns-zone.name}"
+  managed_zone = "${var.dns_zone_name != "none" ? data.google_dns_managed_zone.dns-zone.name : ""}"
   name         = "${format("${var.owner}-${var.service}-priv-%02d.%s",count.index+1,data.google_dns_managed_zone.dns-zone.dns_name)}"
   type         = "A"
   ttl          = "${var.dns_ttl}"
