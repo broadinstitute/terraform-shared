808515ec6326ef2847f61181070270a27cd74a7a
Mark Sarcevicz <smark@broadinstitute.org>
2019-11-12 15:28:29 -0500

    changed syntax again

--- terraform-modules/external-dns/dns.tf
+++ terraform-modules/external-dns/dns.tf
@@ -11,17 +11,11 @@ data "google_dns_managed_zone" "dns_zone" {
 resource "google_dns_record_set" "set_dns_record" {
   iterator = setting
   for_each = [for s in var.records: {
-    name = s.name
+    name = "${s.name}.${data.google_dns_managed_zone.dns_zone.name}"
     type = s.type
     rrdatas = s.rrdatas
-  }]
-
-  content {
-    name = "${setting.value.name}.${data.google_dns_managed_zone.dns_zone.dns_name}"
-    type = setting.value.type
-    rrdatas = setting.value.rrdatas
     provider     = "google.targetdns"
     ttl          = "300"
     managed_zone = data.google_dns_managed_zone.dns_zone.name
-  }
+  }]
 }
