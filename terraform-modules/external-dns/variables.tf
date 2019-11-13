ba518a835f48875b7440c0081a8fcdefbb9aa88b
Mark Sarcevicz <smark@broadinstitute.org>
2019-11-13 13:32:42 -0500

    added var types

--- terraform-modules/external-dns/variables.tf
+++ terraform-modules/external-dns/variables.tf
@@ -1,10 +1,20 @@
 # google project
-variable "target_project" {}
+variable "target_project" {
+  type = string
+}
 #region
-variable "region" {}
+variable "region" {
+  type = string
+}
 #credentials
-variable "target_credentials" {}
+variable "target_credentials" {
+  type = string
+}
 #name of external dns_zone name
-variable "target_dns_zone_name" {}
+variable "target_dns_zone_name" {
+  type = string
+}
 #name and destination values ie: record = 192.168.44.22
-variable "records" {}
+variable "records" {
+  type = map(object({type = string, rrdatas = string}))
+}
