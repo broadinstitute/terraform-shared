da5e60669019acaa4953430a30154b949a117aeb
Henry Ferrara <ferrara@broadinstitute.org>
2019-11-25 11:07:55 -0500

    add project to all resources also fix network name add project to cloudsql module fix project for docker data disk fix empty element

--- terraform-modules/docker-instance-data-disk/instance-data-disk.tf
+++ terraform-modules/docker-instance-data-disk/instance-data-disk.tf
@@ -1,6 +1,7 @@
 
 resource "google_compute_disk" "instance-data-disk" {
    provider                = "google.target"
+   project =  "${var.project}"
    count = "${var.enable_flag == "1"?var.instance_num_hosts:0}"
    name = "${format("%s-%02d-data-disk", var.instance_name, count.index+1)}"
    size = "${var.instance_data_disk_size}"
