d3054436145fac65909d608eee611fae536a00c9
Henry Ferrara <ferrara@broadinstitute.org>
2019-12-05 17:23:27 -0500

    new vpc-network module

---
+++ terraform-modules/vpc-network/network.tf
@@ -0,0 +1,34 @@
+resource "google_compute_network" "vpc-network" {          
+  provider                = "google-beta.target"                                  
+  project                 = var.google_project
+  name                    = var.network_name
+  depends_on              = [module.enable-services]                
+  auto_create_subnetworks = false
+}
+
+resource "google_compute_subnetwork" "subnetwork" {
+  for_each      = var.subnets
+
+  provider                 = google-beta.target
+  project                  = var.google_project
+  name                     = var.network_name
+  network                  = google_compute_network.vpc-network.self_link
+  region                   = each.key
+  ip_cidr_range            = each.value.cidr
+  private_ip_google_access = each.value.private_ip_access
+
+  # until Google provider is at version 3.0+ we still need to supply enable_flow_logs 
+  # value otherwise provider sets it to false and ignores the log_config block even 
+  # though the deprecated message indicates otherwise
+  enable_flow_logs         = each.value.vpc_logging
+
+  dynamic "log_config" {
+    for_each = each.value.vpc_logging ? ["Placehold object so for_each works"] : []
+    content {
+      aggregation_interval = var.aggregation_interval
+      flow_sampling        = var.flow_sampling
+      metadata             = var.metadata
+    }
+  }
+}
+
---
+++ terraform-modules/vpc-network/variables.tf
@@ -0,0 +1,139 @@
+provider "google" {
+  alias = "target"
+}
+
+variable "google_project" {}
+
+provider "google-beta" {
+  alias = "target"
+}
+
+variable "network_name" {
+  type = "string"
+  default = "app-services"
+}
+
+variable "aggregation_interval" {
+  type = string
+  default = "INTERVAL_10_MIN"
+}
+
+variable "flow_sampling" {
+  type = number
+  default = 0.5
+}
+
+variable "metadata" {
+  type = string
+  default = "INCLUDE_ALL_METADATA"
+}
+
+variable "subnets" {
+  type = map(object({
+    cidr = string,
+    private_ip_access = bool,
+    vpc_logging = bool,
+  }))
+  default = {
+    "us-central1" = {
+      cidr = "10.128.0.0/20"
+      private_ip_access = false,
+      vpc_logging = false,
+    },
+    "europe-west1" = {
+      cidr = "10.132.0.0/20"
+      private_ip_access = false,
+      vpc_logging = false,
+    },
+    "us-west1" = {
+      cidr = "10.138.0.0/20"
+      private_ip_access = false,
+      vpc_logging = false,
+    },
+    "asia-east1" = {
+      cidr = "10.140.0.0/20"
+      private_ip_access = false,
+      vpc_logging = false,
+    },
+    "us-east1" = {
+      cidr = "10.142.0.0/20"
+      private_ip_access = false,
+      vpc_logging = false,
+    },
+    "asia-northeast1" = {
+      cidr = "10.146.0.0/20"
+      private_ip_access = false,
+      vpc_logging = false,
+    },
+    "asia-southeast1" = {
+      cidr = "10.148.0.0/20"
+      private_ip_access = false,
+      vpc_logging = false,
+    },
+    "us-east4" = {
+      cidr = "10.150.0.0/20"
+      private_ip_access = false,
+      vpc_logging = false,
+    },
+    "australia-southeast1" = {
+      cidr = "10.152.0.0/20"
+      private_ip_access = false,
+      vpc_logging = false,
+    },
+    "europe-west2" = {
+      cidr = "10.154.0.0/20"
+      private_ip_access = false,
+      vpc_logging = false,
+    },
+    "europe-west3" = {
+      cidr = "10.156.0.0/20"
+      private_ip_access = false,
+      vpc_logging = false,
+    },
+    "southamerica-east1" = {
+      cidr = "10.158.0.0/20"
+      private_ip_access = false,
+      vpc_logging = false,
+    },
+    "asia-south1" = {
+      cidr = "10.160.0.0/20"
+      private_ip_access = false,
+      vpc_logging = false,
+    },
+    "northamerica-northeast1" = {
+      cidr = "10.162.0.0/20"
+      private_ip_access = false,
+      vpc_logging = false,
+    },
+    "europe-west4" = {
+      cidr = "10.164.0.0/20"
+      private_ip_access = false,
+      vpc_logging = false,
+    },
+    "europe-north1" = {
+      cidr = "10.166.0.0/20"
+      private_ip_access = false,
+      vpc_logging = false,
+    },
+    "us-west2" = {
+      cidr = "10.168.0.0/20"
+      private_ip_access = false,
+      vpc_logging = false,
+    },
+    "asia-east2" = {
+      cidr = "10.170.0.0/20"
+      private_ip_access = false,
+      vpc_logging = false,
+    },
+    "europe-west6" = {
+      cidr = "10.172.0.0/20"
+      private_ip_access = false,
+      vpc_logging = false,
+    },
+    "asia-northeast2" = {
+      cidr = "10.174.0.0/20"
+      private_ip_access = false,
+      vpc_logging = false,
+    },
+  }
+}
