6609be9c981e7e5773bfbfac3d9cc703c1e7b605
Mike Flinn <mflinn@broadinstitute.org>
2020-02-21 16:05:25 -0500

    Created wireframe main.tf for SD uptime check

---
+++ terraform-modules/stackdriver/http-uptime/main.tf
@@ -0,0 +1,19 @@
+
+resource google_monitoring_uptime_check_config http_check {
+  display_name = var.check_name
+  timeout = var.check_timeout
+  period = var.check_frequency
+
+  http_check {
+    path = var.url_path
+    port = var.port
+  }
+
+  monitored_resource {
+    type = "uptime_url"
+    labels = {
+      project_id = var.project
+      host = var.domain_name
+    }
+  }
+}
