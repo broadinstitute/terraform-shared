22823ea05beb00659ce824736c32cf51acd274bf
Mark Sarcevicz <smark@broadinstitute.org>
2020-02-25 15:23:56 -0500

    fixed for vault verison

--- terraform-modules/vault-approle/main.tf
+++ terraform-modules/vault-approle/main.tf
@@ -1,7 +1,7 @@
 resource "vault_approle_auth_backend_role" "approle" {
   for_each  = var.approle
   role_name      = "${each.key}"
-  token_policies = each.value["token_policies"]
+  policies = each.value["token_policies"]
   secret_id_num_uses = lookup(each.value, "secret_id_num_uses", null)
   secret_id_ttl = lookup(each.value, "secret_id_ttl", null)
   token_num_uses = lookup(each.value, "token_num_uses", null)
