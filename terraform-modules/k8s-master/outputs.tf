e3348f6aec18f76c80a224e9fca65136507e2811
Henry Ferrara <ferrara@broadinstitute.org>
2020-03-08 17:03:15 -0400

    added instance key

--- terraform-modules/k8s-master/outputs.tf
+++ terraform-modules/k8s-master/outputs.tf
@@ -1,11 +1,11 @@
 output name {
-  value = google_container_cluster.cluster.name
+  value = google_container_cluster.cluster[0].name
 }
 
 output endpoint {
-  value = google_container_cluster.cluster.endpoint
+  value = google_container_cluster.cluster[0].endpoint
 }
 
 output ca_certificate {
-  value = google_container_cluster.cluster.master_auth[0].cluster_ca_certificate
+  value = google_container_cluster.cluster[0].master_auth[0].cluster_ca_certificate
 }
