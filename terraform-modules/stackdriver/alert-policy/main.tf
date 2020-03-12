bc19a17eeafdbf9e4046b27b49bced83382738fd
Mike Flinn <mflinn@broadinstitute.org>
2020-03-12 17:35:25 -0400

    Added notes for skeleton module structure

--- terraform-modules/stackdriver/alert-policy/main.tf
+++ terraform-modules/stackdriver/alert-policy/main.tf
@@ -11,7 +11,8 @@ resource google_monitoring_alert_policy cluster_alert {
     # Recommendation is use to use more policies with closely related conditions
 
     # name to be associated with the condition for display in gcloud
-    display_name = var.condition_name
+    
+    #Contains list of condition objects    
 
     condition_absent {
       # Specifiy condition behavior if metric data is missing
@@ -20,6 +21,7 @@ resource google_monitoring_alert_policy cluster_alert {
     condition_threshold {
       # Specify circumstances where alert is triggered
     }
+    ## Possibly use for loop syntax to easily allow for multiple conditions. (Decide if this is a pattern we should support)
   }
 
   enabled = true
