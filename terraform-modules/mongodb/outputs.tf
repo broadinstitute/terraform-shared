ce9c721a4160ad55eadb6b4f9a2b1a32c6063bb1
Greg Malkov <gmalkov@broadinstitute.org>
2020-03-12 12:46:13 -0400

    make URI outputs sensitive

--- terraform-modules/mongodb/outputs.tf
+++ terraform-modules/mongodb/outputs.tf
@@ -20,10 +20,12 @@ output "instance_priv_hostnames" {
 
 output "mongo_uri" {
   value = var.dns_zone_name != "none" ? "mongodb://${var.mongodb_app_username}:${var.mongodb_app_password}@${ join(",", data.null_data_source.hostnames_with_no_trailing_dot.*.outputs.hostname) }/${var.mongodb_database}" : "mongodb://${var.mongodb_app_username}:${var.mongodb_app_password}@${ join(",", module.instances.instance_public_ips) }/${var.mongodb_database}"
+  sensitive = true
 }
 
 output "mongo_priv_uri" {
   value = var.dns_zone_name != "none" ? "mongodb://${var.mongodb_app_username}:${var.mongodb_app_password}@${ join(",", data.null_data_source.hostnames_with_no_trailing_dot.*.outputs.hostname_priv) }/${var.mongodb_database}" : "mongodb://${var.mongodb_app_username}:${var.mongodb_app_password}@${ join(",", module.instances.instance_private_ips) }/${var.mongodb_database}"
+  sensitive = true
 }
 
 output "instance_instance_group" {
