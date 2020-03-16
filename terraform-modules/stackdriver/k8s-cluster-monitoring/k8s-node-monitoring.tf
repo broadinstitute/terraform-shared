5d37904735cc0fc5ddd53f8d1e0e8760b8fa539f
Mike Flinn <mflinn@broadinstitute.org>
2020-03-16 16:45:59 -0400

    Terraform linter formatting changes

--- terraform-modules/stackdriver/k8s-cluster-monitoring/k8s-node-monitoring.tf
+++ terraform-modules/stackdriver/k8s-cluster-monitoring/k8s-node-monitoring.tf
@@ -1,36 +1,34 @@
 resource google_monitoring_alert_policy node_resource_alerts {
 
-  provider = google.target
-
+  provider     = google.target
   display_name = "k8s-node-resource-usage-alert"
 
   # Valid option are "AND" or "OR"
   combiner = var.condition_combine_method
-  project = var.project
+  project  = var.project
 
   conditions {
     # name to be associated with the condition for display in gcloud
-    display_name = "node-cpu-utilization"    
+    display_name = "node-cpu-utilization"
 
     # condition_absent {
     #   # Specifiy condition behavior if metric data is missing
     # } 
 
     condition_threshold {
+
       # Specify circumstances where alert is triggered
-
       threshold_value = var.node_cpu_threshold
-
-      comparison = var.threshold_comparison.greater_than
-      duration = var.node_threshold_duration
+      comparison      = var.threshold_comparison.greater_than
+      duration        = var.node_threshold_duration
 
       # Specify api path of metrics and any resource filters to apply
       filter = var.node_cpu_metric
 
       aggregations {
-        # group_by_fields = ["metric.label.\"instance_name\""]
-        per_series_aligner = var.series_align_method 
-        alignment_period = var.alignment_period
+
+        per_series_aligner = var.series_align_method
+        alignment_period   = var.alignment_period
       }
     }
   }
@@ -39,18 +37,18 @@ resource google_monitoring_alert_policy node_resource_alerts {
     display_name = "node-memory-utilization"
 
     condition_threshold {
-      threshold_value = var.node_memory_threshold
 
-      comparison = var.threshold_comparison.greater_than
-      duration = var.node_threshold_duration
+      threshold_value = var.node_memory_threshold
+      comparison      = var.threshold_comparison.greater_than
+      duration        = var.node_threshold_duration
 
       filter = var.node_memory_metric
 
       aggregations {
-        per_series_aligner = var.series_align_method
-        alignment_period = var.alignment_period
+        per_series_aligner   = var.series_align_method
+        alignment_period     = var.alignment_period
         cross_series_reducer = var.reducer_method.sum
-        group_by_fields = ["${var.group_by_labels.node_name}"]
+        group_by_fields      = ["${var.group_by_labels.node_name}"]
       }
     }
   }
@@ -65,7 +63,7 @@ resource google_monitoring_alert_policy node_resource_alerts {
 
   documentation {
     # Information to be displayed in a dashboard that provides more context for the alert
-    content = "A node in the ${var.cluster_name} cluster has been running at above high resource utilization for greater than ${var.node_threshold_duration}econds"
+    content   = "A node in the ${var.cluster_name} cluster has been running at above high resource utilization for greater than ${var.node_threshold_duration}econds"
     mime_type = "text/markdown"
   }
 }
--- terraform-modules/stackdriver/k8s-cluster-monitoring/variables.tf
+++ terraform-modules/stackdriver/k8s-cluster-monitoring/variables.tf
@@ -1,77 +1,77 @@
 # Required Variables
 
 variable "policy_name" {
-  type = string
+  type        = string
   description = "Display name to be associated with a particular alerting policy"
 }
 
 variable "condition_combine_method" {
-  type = string
+  type        = string
   description = "logic to trigger the alert when multiple conditions are present only 'AND' and 'OR' are valid arguments"
 }
 
 variable "project" {
-  type = string
+  type        = string
   description = "The GCP project the alert policy should be created under"
 }
 
 variable "cluster_name" {
-  type = string
+  type        = string
   description = " Name of the cluster the alert policy will monitor"
 }
 
 # Optional Variables 
 
 variable "notification_channels" {
-  type = list(string)
-  default = []
+  type        = list(string)
+  default     = []
   description = "List of channels that should be pinged when alert fires. Array entries must be of form: projects/[PROJECT_ID]/notificationChannels/[CHANNEL_ID]"
 
 }
 
 variable "node_cpu_threshold" {
-  type = number
-  default = 0.95
+  type        = number
+  default     = 0.95
   description = "The threshold the cpu utilization on a node must be above to trigger the alert. Represents a fraction and must be between 0 and 1"
 }
 
 variable "node_memory_threshold" {
-  type = number
-  default = 0.95
+  type        = number
+  default     = 0.95
   description = "The threshold memory utilization on a node to trigger an alert. Represents a fraction of allocatble memory and must be between 0 and 1"
 }
 
 variable "threshold_comparison" {
   type = object({
-    less_than = string
+    less_than    = string
     greater_than = string
   })
 
   default = {
-    less_than = "COMPARISON_LT"
+    less_than    = "COMPARISON_LT"
     greater_than = "COMPARISON_GT"
   }
 }
 
 variable "node_threshold_duration" {
-  type = string
-  default = "300s"
+  type        = string
+  default     = "300s"
   description = "The amount of time a metric must be above a threshold in order to trigger alert. Values must be in multiples of 60"
 }
 
 variable "series_align_method" {
-  type = string
-  default = "ALIGN_MEAN"
+  type        = string
+  default     = "ALIGN_MEAN"
   description = "Method for filling in time series data between collected data points. See stackdriver docs for more options"
 }
 
 variable "alignment_period" {
-  type = string
-  default = "60s"
+  type        = string
+  default     = "60s"
   description = "Interval at which datapoints are sent to gcloud monitoring. Data between periods is managed series_align_method"
 }
 
-variable "reducer_method" { 
+variable "reducer_method" {
   default = {
     sum = "REDUCE_SUM"
   }
@@ -91,13 +91,13 @@ variable "group_by_labels" {
 # Note: Do not adjust values below without consulting with DevOps
 
 variable "node_cpu_metric" {
-  type = string
-  default = "metric.type=\"compute.googleapis.com/instance/cpu/utilization\" AND resource.type=\"gce_instance\""
+  type        = string
+  default     = "metric.type=\"compute.googleapis.com/instance/cpu/utilization\" AND resource.type=\"gce_instance\""
   description = "Metric used for tracking node cpu utilization"
 }
 
 variable "node_memory_metric" {
-  type = string
-  default = "metric.type=\"kubernetes.io/node/memory/allocatable_utilization\" resource.type=\"k8s_node\""
+  type        = string
+  default     = "metric.type=\"kubernetes.io/node/memory/allocatable_utilization\" resource.type=\"k8s_node\""
   description = "Metric used for tracking allocatable memory available per node"
 }
