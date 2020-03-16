a1ad8ec080b60e8533f598cbdb8440806741008e
Mike Flinn <mflinn@broadinstitute.org>
2020-03-16 15:01:49 -0400

    Refactored node monitoroing policies to use variables instead of hard coded values

--- terraform-modules/stackdriver/alert-policy/k8s-node-monitoring.tf
+++ terraform-modules/stackdriver/alert-policy/k8s-node-monitoring.tf
@@ -17,18 +17,18 @@ resource google_monitoring_alert_policy node_resource_alerts {
     condition_threshold {
       # Specify circumstances where alert is triggered
 
-      threshold_value = 0.95
+      threshold_value = var.node_cpu_threshold
 
-      comparison = "COMPARISON_GT"
-      duration = "300s"
+      comparison = var.threshold_comparison.greater_than
+      duration = var.node_threshold_duration
 
       # Specify api path of metrics and any resource filters to apply
-      filter = "metric.type=\"compute.googleapis.com/instance/cpu/utilization\" AND resource.type=\"gce_instance\""
+      filter = var.node_cpu_metric
 
       aggregations {
         # group_by_fields = ["metric.label.\"instance_name\""]
-        per_series_aligner = "ALIGN_MEAN" 
-        alignment_period = "60s"
+        per_series_aligner = var.series_align_method 
+        alignment_period = var.alignment_period
 
       }
 
@@ -39,18 +39,18 @@ resource google_monitoring_alert_policy node_resource_alerts {
     display_name = "node-memory-utilization"
 
     condition_threshold {
-      threshold_value = 0.95
+      threshold_value = var.node_memory_threshold
 
-      comparison = "COMPARISON_GT"
-      duration = "300s"
+      comparison = var.threshold_comparison.greater_than
+      duration = var.node_threshold_duration
 
-      filter = "metric.type=\"kubernetes.io/node/memory/allocatable_utilization\" resource.type=\"k8s_node\""
+      filter = var.node_memory_metric
 
       aggregations {
-        per_series_aligner = "ALIGN_MEAN"
-        alignment_period = "60s"
-        cross_series_reducer = "REDUCE_SUM"
-        group_by_fields = ["resource.labels.node_name"]
+        per_series_aligner = var.series_align_method
+        alignment_period = var.alignment_period
+        cross_series_reducer = var.reducer_method.sum
+        group_by_fields = ["${var.group_by_labels.node_name}"]
       }
     }
   }
@@ -65,7 +65,7 @@ resource google_monitoring_alert_policy node_resource_alerts {
 
   documentation {
     # Information to be displayed in a dashboard that provides more context for the alert
-    content = "A kubernetes node has been utilizizing more resources than expected for an extneded period of time"
+    content = "A node in the ${var.cluster_name} cluster has been running at above high resource utilization for greater than ${var.node_threshold_duration}econds"
     mime_type = "text/markdown"
   }
 }
--- terraform-modules/stackdriver/alert-policy/variables.tf
+++ terraform-modules/stackdriver/alert-policy/variables.tf
@@ -28,3 +28,76 @@ variable "notification_channels" {
   description = "List of channels that should be pinged when alert fires. Array entries must be of form: projects/[PROJECT_ID]/notificationChannels/[CHANNEL_ID]"
 
 }
+
+variable "node_cpu_threshold" {
+  type = number
+  default = 0.95
+  description = "The threshold the cpu utilization on a node must be above to trigger the alert. Represents a fraction and must be between 0 and 1"
+}
+
+variable "node_memory_threshold" {
+  type = number
+  default = 0.95
+  description = "The threshold memory utilization on a node to trigger an alert. Represents a fraction of allocatble memory and must be between 0 and 1"
+}
+
+variable "threshold_comparison" {
+  type = object({
+    less_than = string
+    greater_than = string
+  })
+
+  default = {
+    less_than = "COMPARISON_LT"
+    greater_than = "COMPARISON_GT"
+  }
+}
+
+variable "node_threshold_duration" {
+  type = string
+  default = "300s"
+  description = "The amount of time a metric must be above a threshold in order to trigger alert. Values must be in multiples of 60"
+}
+
+variable "series_align_method" {
+  type = string
+  default = "ALIGN_MEAN"
+  description = "Method for filling in time series data between collected data points. See stackdriver docs for more options"
+}
+
+variable "alignment_period" {
+  type = string
+  default = "60s"
+  description = "Interval at which datapoints are sent to gcloud monitoring. Data between periods is managed series_align_method"
+}
+
+variable "reducer_method" { 
+  default = {
+    sum = "REDUCE_SUM"
+  }
+  description = "Mapping of time series aggregator methods to their correct name in gcloud monitoring api. Further documentation can be found in gcloud monitoring docs"
+}
+
+variable "group_by_labels" {
+  default = {
+    node_name = "resource.labels.node_name"
+  }
+  description = "Mapping of monitored resource labels to their gcloud monitoring api name. Used for aggregating timeseries data together"
+}
+
+
+###### Monitoring Metric Definitions ######
+
+# Note: Do not adjust values below without consulting with DevOps
+
+variable "node_cpu_metric" {
+  type = string
+  default = "metric.type=\"compute.googleapis.com/instance/cpu/utilization\" AND resource.type=\"gce_instance\""
+  description = "Metric used for tracking node cpu utilization"
+}
+
+variable "node_memory_metric" {
+  type = string
+  default = "metric.type=\"kubernetes.io/node/memory/allocatable_utilization\" resource.type=\"k8s_node\""
+  description = "Metric used for tracking allocatable memory available per node"
+}
