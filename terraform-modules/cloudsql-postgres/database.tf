c6cdec6a9f920f35fb2a00ba4a730f653dc86728
Greg Malkov <51164901+gmalkov@users.noreply.github.com>
2020-03-22 23:55:05 -0400

    update psql module (#80)

--- terraform-modules/cloudsql-postgres/database.tf
+++
@@ -1,7 +0,0 @@
-resource "google_sql_database" "app-database" {
-  provider              = "google.target"
-  count = "${var.enable_flag}"
-  name = "${var.cloudsql_database_name}"
-  instance  = "${var.enable_flag ? google_sql_database_instance.cloudsql-instance.0.name : ""}"
-  depends_on = ["google_sql_database_instance.cloudsql-instance" ]
-}
--- terraform-modules/cloudsql-postgres/outputs.tf
+++ terraform-modules/cloudsql-postgres/outputs.tf
@@ -1,8 +1,24 @@
-
-output "cloudsql-public-ip" {
-  value = "${google_sql_database_instance.cloudsql-instance.0.first_ip_address}"
+output "public_ip" {
+  value = google_sql_database_instance.cloudsql_instance.first_ip_address
 }
 
-output "cloudsql-instance-name" {
-  value = "${google_sql_database_instance.cloudsql-instance.0.name}"
+output "instance_name" {
+  value = google_sql_database_instance.cloudsql_instance.name
+}
+
+output "root_user_password" {
+  value = random_id.root_user_password.hex
+  sensitive = true
+}
+
+output "app_db_creds" {
+  value = {
+    for db in keys(google_sql_database.app_database):
+    db => {
+      db = google_sql_database.app_database[db].name
+      username = google_sql_user.app_user[db].name
+      password = google_sql_user.app_user[db].password
+    }
+  }
+  sensitive = true
 }
