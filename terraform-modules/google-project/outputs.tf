ff5a5410e860e7b0aa708ec6e630311da2d34f2e
Hussein Elgridly <hussein@broadinstitute.org>
2020-03-24 17:48:10 -0400

    output google service account things

--- terraform-modules/google-project/outputs.tf
+++ terraform-modules/google-project/outputs.tf
@@ -5,3 +5,11 @@ output "project_number" {
 output "project_name" {
   value = google_project.project.name
 }
+
+output "service_accounts_with_keys" {
+  value = google_service_account.service-accounts-with-keys.email
+}
+
+output "service_accounts_without_keys" {
+  value = google_service_account.service-accounts-without-keys.email
+}
