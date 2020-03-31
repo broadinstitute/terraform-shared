80deff89cc67db2bbfaf27bc2da4f98c5ce32fdd
Mike Flinn <mflinn@broadinstitute.org>
2020-03-31 13:58:38 -0400

    Successfully configured uptime check for gae app.

--- terraform-modules/stackdriver/gae-monitoring/gae-uptime.tf
+++ terraform-modules/stackdriver/gae-monitoring/gae-uptime.tf
@@ -1,13 +1,12 @@
-resource google_monitoring_uptime_check_ocnfig gae_uptime_check {
+resource google_monitoring_uptime_check_config gae_uptime_check {
   provider     = google.target
-  project      = var.workspace_host_project
   display_name = "${var.service_name}-uptime-check"
-  timeout      = "30s"
+  timeout      = "10s"
   period       = "60s"
 
   http_check {
-    path = var.uptime_api_path
-    port = var.port
+    path = var.gae_api_path
+    port = var.https_enabled ? "443" : "80"
     # Used to toggle http vs https, default to http
     use_ssl      = var.https_enabled
     validate_ssl = var.https_enabled
@@ -16,21 +15,20 @@ resource google_monitoring_uptime_check_ocnfig gae_uptime_check {
   monitored_resource {
     type = "uptime_url"
     labels = {
-      project_id = var.project
-      host       = var.gae_app_domain
+      project_id = var.gae_host_project
+      host       = var.gae_domain
     }
   }
 }
 
 locals {
-  uptime_metric = "metric.type=\"monitoring.googleapis.com/uptime_check/check_passed\" resource.type=\"uptime_url\" metric.label.\"check_id\"="
+  uptime_metric = "metric.type=\"monitoring.googleapis.com/uptime_check/check_passed\" resource.type=\"uptime_url\" resource.label.\"host\"=\"${var.gae_domain}\""
 }
 
 resource google_monitoring_alert_policy gae-uptime-alert {
   provider = google.target
 
   display_name          = "${var.service_name}-uptime-alert"
-  project               = var.workspace_host_project
   combiner              = "OR"
   enabled               = true
   notification_channels = var.notification_channels
@@ -48,13 +46,12 @@ resource google_monitoring_alert_policy gae-uptime-alert {
       comparison      = "COMPARISON_LT"
       duration        = "300s"
 
-      filter = "${local.uptime_metric}=\"${gae_uptime_check.uptime_check_id}\""
+      filter = local.uptime_metric
 
       aggregations {
-        per_series_aligner   = var.serires_align_method
+        per_series_aligner   = var.series_align_method.count_true
         alignment_period     = var.alignment_period
-        cross_series_reducer = var.reducer_method.sum
-        group_by_fields      = [var.group_by_labels.check_id]
+        cross_series_reducer = var.reducer_method.none
       }
     }
   }
