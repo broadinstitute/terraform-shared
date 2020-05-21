c822e2ba4e48e953a74628c81ed6a4c74176fc71
Fab-T <fabien.traquet@gmail.com>
2020-05-21 14:21:15 -0400

    fix output

--- terraform-modules/storage-bucket/outputs.tf
+++ terraform-modules/storage-bucket/outputs.tf
@@ -1,24 +1,14 @@
-output "buckets" {
+output "bucket" {
   description = "Bucket resources."
   value       = google_storage_bucket.bucket
 }
 
-output "names" {
-  description = "Bucket names."
-  value       = zipmap(var.bucket_name, slice(google_storage_bucket.bucket[*].name, 0, length(var.bucket_name)))
+output "name" {
+  description = "Bucket name."
+  value       = google_storage_bucket.bucket.name
 }
 
-output "urls" {
+output "url" {
   description = "Bucket URLs."
-  value       = zipmap(var.bucket_name, slice(google_storage_bucket.bucket[*].url, 0, length(var.bucket_name)))
-}
-
-output "names_list" {
-  description = "List of bucket names."
-  value       = google_storage_bucket.bucket[*].name
-}
-
-output "urls_list" {
-  description = "List of bucket URLs."
-  value       = google_storage_bucket.bucket[*].url
+  value       = google_storage_bucket.bucket.url
 }
