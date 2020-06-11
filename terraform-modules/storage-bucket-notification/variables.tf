a19b3ee969e174121ed9468f45d6a16c77a94827
Saman Ehsan <se8ea@virginia.edu>
2020-06-11 17:59:17 -0400

    Add bucket notification module
    The module creates a pubsub topic
    and configures a bucket to publish to
    the topic on file upload events

---
+++ terraform-modules/storage-bucket-notification/variables.tf
@@ -0,0 +1,9 @@
+variable "bucket_name" {
+  description = "The name of the bucket."
+  type = string
+}
+
+variable "topic_name" {
+  description = "The name of the pub/sub topic."
+  type = string
+}
