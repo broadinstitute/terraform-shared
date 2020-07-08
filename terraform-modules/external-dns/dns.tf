18ef4bc210c2f96a833e132d038b9f71d9f26b1c
Mark Sarcevicz <smark@broadinstitute.org>
2020-07-08 10:27:11 -0400

    added period

--- terraform-modules/external-dns/dns.tf
+++ terraform-modules/external-dns/dns.tf
@@ -14,7 +14,7 @@ resource google_dns_record_set a_dns {
   ttl = "300"
 
   managed_zone = var.zone_gcp_name
-  name = "${each.value}-global.${var.zone_dns_name}"
+  name = "${each.value}-global.${var.zone_dns_name}."
   rrdatas = [google_compute_global_address.global_ip_address[each.value].address]
   depends_on = [var.dependencies]
 }
@@ -27,7 +27,7 @@ resource google_dns_record_set cname_dns {
   ttl = "300"
 
   managed_zone = var.zone_gcp_name
-  name = "${each.value}.${var.zone_dns_name}"
+  name = "${each.value}.${var.zone_dns_name}."
   rrdatas = [google_dns_record_set.a_dns[each.value].name]
   depends_on = [var.dependencies]
 }
