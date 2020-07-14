11f1e0b279d29d4378d5e2060fea9353fa1214de
Mark Sarcevicz <smark@broadinstitute.org>
2020-07-14 10:51:22 -0400

    added authenicated users

--- terraform-modules/gcs_bq_log_sink/log_sinks.tf
+++ terraform-modules/gcs_bq_log_sink/log_sinks.tf
@@ -21,7 +21,7 @@ resource "google_bigquery_dataset" "logs" {
 resource "google_bigquery_dataset_access" "access" {
   dataset_id    = google_bigquery_dataset.logs[0].dataset_id
   role          = "OWNER"
-  user_by_email = "${google_logging_project_sink.bigquery-log-sink[0].writer_identity}"
+  special_group = "allAuthenticatedUsers"
   depends_on    = [google_bigquery_dataset.logs,google_logging_project_sink.bigquery-log-sink]
 }
 
