2cf3857d3ee259704b1bafc869670aff698c57d0
mflinn-broad <mflinn@broadinstitute.org>
2020-08-12 13:23:25 -0400

    add project field

--- terraform-modules/stackdriver/uptime-check/uptime.tf
+++ terraform-modules/stackdriver/uptime-check/uptime.tf
@@ -2,6 +2,7 @@ resource "google_monitoring_uptime_check_config" "uptime_check" {
   count = var.enabled ? 1 : 0
 
   display_name = local.check_name
+  project      = var.google_project
   timeout      = var.timeout
 
   http_check {
