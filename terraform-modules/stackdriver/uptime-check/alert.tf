d0a8c580a693540b4b4bafca2315771a25369a82
mflinn-broad <mflinn@broadinstitute.org>
2020-08-17 16:40:19 -0400

    Add alert on uptime check failure

---
+++ terraform-modules/stackdriver/uptime-check/alert.tf
@@ -0,0 +1,30 @@
+locals {
+  uptime_metric = "metric.type=\"monitoring.googleapis.com/uptime_check/check_passed\" resource.type=\"uptime_url\" metric.label.\"check_id\"=\"${google_monitoring_uptime_check_config.uptime_check.uptime_check_id}\""
+}
+
+resource "google_monitoring_alert_policy" "uptime_alert" {
+  count    = var.enabled ? 1 : 0
+  provider = google.target
+
+  display_name          = "${var.service}-unavailable"
+  combiner              = "OR"
+  notification_channels = []
+
+  conditions {
+    display_name = "${var.service}-uptime"
+
+    condition_threshold {
+      threshold_value = 1
+      comparison      = "GREATER_THAN"
+      duration        = "300s"
+      filter          = local.uptime_metric
+
+      aggregations {
+        per_series_aligner   = "ALIGN_NEXT_OLDER"
+        alignment_period     = "600s"
+        group_by_fields      = []
+        cross_series_reducer = "REDUCE_NONE"
+      }
+    }
+  }
+}
--- terraform-modules/stackdriver/uptime-check/notification-channel.tf
+++ terraform-modules/stackdriver/uptime-check/notification-channel.tf
@@ -1,5 +1,15 @@
-data "google_monitoring_notification_channel" "slack_channel" {
-  display_name = var.channel_name
-  type         = var.alert_type
-  provider     = google.target
-}
+# # data "google_monitoring_notification_channel" "slack_channel" {
+# #   display_name = var.channel_name
+# #   type         = var.alert_type
+# #   provider     = google.target
+# # }
+
+# resource "google_monitoring_notification_channel" "slack_channel" {
+#   type = "slack"
+#   labels = {
+#     "channel_name" = var.channel_name
+#   }
+#   sensitive_labels {
+#     auth_token = 
+#   }
+# }
