68242bfcfa29effe670c326c4cd5b834c05205dd
Henry Ferrara <ferrara@broadinstitute.org>
2020-08-19 09:41:58 -0400

    try defining objects

--- terraform-modules/vault-approle/varibles.tf
+++ terraform-modules/vault-approle/varibles.tf
@@ -1,5 +1,6 @@
 variable "approle" {
-  type = map(object({token_policies = list(string)}))
+#  type = map(object({token_policies = list(string)}))
+  type = map(object({ token_policies = list(string), secret_id_num_uses = number, secret_id_ttl = number, token_ttl = number, token_num_uses = number, token_max_ttl = number }))
 }
 
 variable "vault_address" {
