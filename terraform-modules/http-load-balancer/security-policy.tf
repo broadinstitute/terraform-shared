4e3e43241a416c005684561ee4435849abf1edee
Greg Malkov <gmalkov@broadinstitute.org>
2020-08-25 12:47:56 -0400

    add default deny rule

--- terraform-modules/http-load-balancer/security-policy.tf
+++ terraform-modules/http-load-balancer/security-policy.tf
@@ -4,6 +4,18 @@ resource "google_compute_security_policy" "policy" {
 
   name = "${var.load_balancer_name}-policy"
 
+  rule {
+    action   = "deny"
+    priority = "2147483647"
+    match {
+      versioned_expr = "SRC_IPS_V1"
+      config {
+        src_ip_ranges = ["*"]
+      }
+    }
+    description = "default deny rule"
+  }
+
   dynamic "rule" {
     for_each = var.load_balancer_rules
     content {
