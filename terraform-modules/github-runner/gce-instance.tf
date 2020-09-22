59ac44a89665d24fb0bf8d890d156521a2a259e3
Jack Warren <jwarren@broadinstitute.org>
2020-09-22 16:18:46 -0400

    Improve naming scheme

--- terraform-modules/github-runner/gce-instance.tf
+++ terraform-modules/github-runner/gce-instance.tf
@@ -1,26 +1,47 @@
-resource "random_id" "id" {
-  byte_length = 8
+# Unique per module, to group the resources
+resource "random_id" "base-id" {
+  byte_length = 4
 }
 
-locals {
-  base-name = substr("gh-runner-${var.repo}-${random_id.id.hex}", 0, 58)
+# Additional name component for the runner to force
+# recreation if metadata changes (which would normally
+# not really take effect until a restart)
+resource "random_id" "runner-id" {
+  keepers = {
+    metadata = {
+      role-id-path           = var.vault-role-id-path
+      secret-id-path         = var.vault-secret-id-path
+      vault-server           = var.vault-server
+      github-pat-secret-path = var.github-personal-access-token-path
+      repo                   = "broadinstitute/${var.repo}"
+      shutdown-script        = file("${path.module}/shutdown-script.sh")
+      runner-labels          = join(",", var.runner-labels)
+      actions-user           = var.actions-user
+    }
+  }
+
+  byte_length = 4
 }
 
+# Create a static IP for each runner that won't change even
+# if metadata or something else does
 resource "google_compute_address" "static" {
   count = var.runners
 
-  name = "${local.base-name}-${count.index + 1}-ip"
+  name = "gh-runner-${var.repo}-${random_id.base-id.hex}-ip-${count.index + 1}"
 }
 
+# Base the runner on latest Ubuntu
 data "google_compute_image" "ubuntu" {
   family  = "ubuntu-2004-lts"
   project = "ubuntu-os-cloud"
 }
 
+# Create each runner
 resource "google_compute_instance" "runner" {
   count = var.runners
 
-  name         = "${local.base-name}-${count.index + 1}"
+  name         = "gh-runner-${var.repo}-${random_id.base-id.hex}-${random_id.runner-id.hex}-${count.index + 1}"
   description  = "GitHub Actions runner ${count.index + 1} for broadinstitute/${var.repo}"
   machine_type = var.machine-type
   zone         = var.zone
@@ -49,16 +70,7 @@ resource "google_compute_instance" "runner" {
     auto_delete = true
   }
 
-  metadata = {
-    role-id-path           = var.vault-role-id-path
-    secret-id-path         = var.vault-secret-id-path
-    vault-server           = var.vault-server
-    github-pat-secret-path = var.github-personal-access-token-path
-    repo                   = "broadinstitute/${var.repo}"
-    shutdown-script        = file("${path.module}/shutdown-script.sh")
-    runner-labels          = join(",", var.runner-labels)
-    actions-user           = var.actions-user
-  }
+  metadata = random_id.runner-id.keepers.metadata
 
   metadata_startup_script = file("${path.module}/startup-script.sh")
 }
