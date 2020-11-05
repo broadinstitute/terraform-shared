67cc6d751241564385c3631286f1dbe7d4838c02
Chelsea Hoover <chelsea@broadinstitute.org>
2020-11-05 09:17:34 -0500

    test

--- terraform-modules/k8s-node-pool/main.tf
+++ terraform-modules/k8s-node-pool/main.tf
@@ -42,7 +42,7 @@ resource google_container_node_pool pool {
     # CIS compliance: COS image
     image_type      = "COS"
     machine_type    = var.machine_type
-    disk_size_gb    = var.disk_size_gb
+    disk_size_gb    = 300 # var.disk_size_gb
     service_account = var.service_account
 
     # Protect node metadata
