11b04cbe1673a39b9440b528352323f490dd3b88
rhiananthony <ranthony@broadinstitute.org>
2021-02-04 11:18:18 -0500

    Adding logging to the storage bucket module

--- terraform-modules/storage-bucket/bucket.tf
+++ terraform-modules/storage-bucket/bucket.tf
@@ -13,7 +13,7 @@ resource "google_storage_bucket" "bucket" {
 
   logging {
     log_bucket        = var.log_bucket
-    log_object_prefix = var.log_bucket_prefix
+    log_object_prefix = var.log_object_prefix
   }
 
   dynamic "retention_policy" {
