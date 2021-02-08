8c090485d9258cf35146c89eac6aa670a133eda4
rhiananthony <ranthony@broadinstitute.org>
2021-02-08 13:34:42 -0500

    removed unnecessary load_config_file variable

--- terraform-modules/wfl-instance/namespace.tf
+++ terraform-modules/wfl-instance/namespace.tf
@@ -1,8 +1,6 @@
 data "google_client_config" "current" {}
 
 provider "kubernetes" {
-    load_config_file = false
-
     host  = "https://${var.gke_endpoint}"
     token = data.google_client_config.current.access_token
     cluster_ca_certificate = base64decode(
