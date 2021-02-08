8002d9c7608ace34cbea321c2859f315673ddc0c
rhiananthony <ranthony@broadinstitute.org>
2021-02-08 16:29:15 -0500

    Updating the versions required by the wfl-instance, the firewall-rule and the github-runner modules

--- terraform-modules/github-runner/versions.tf
+++ terraform-modules/github-runner/versions.tf
@@ -2,8 +2,8 @@ terraform {
   required_version = ">= 0.13.6"
 
   required_providers {
-    google      = ">= 3.2.0"
-    google-beta = ">= 3.2.0"
-    random      = ">= 2.2"
+    google      = ">= 3.55.0"
+    google-beta = ">= 3.55.0"
+    random      = ">= 3.0.1"
   }
 }
