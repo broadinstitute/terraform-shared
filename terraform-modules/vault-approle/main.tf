25a5bc9da3aec07232a59c5b0b72c116660b4900
mflinn-broad <mflinn@broadinstitute.org>
2021-06-09 14:09:13 -0400

    reverting change to avoid unpredictable changes in existing app roles

--- terraform-modules/vault-approle/main.tf
+++ terraform-modules/vault-approle/main.tf
@@ -1,13 +1,13 @@
 resource "vault_approle_auth_backend_role" "approle" {
-  for_each  = var.approle
-  role_name      = "${each.key}"
-  policies = each.value["token_policies"]
-  token_no_default_policy = true
-  secret_id_num_uses = lookup(each.value, "secret_id_num_uses", null)
-  secret_id_ttl = lookup(each.value, "secret_id_ttl", null)
-  token_num_uses = lookup(each.value, "token_num_uses", null)
-  token_ttl = lookup(each.value, "token_ttl", null)
-  token_max_ttl = lookup(each.value, "token_max_ttl", null)
+  for_each                = var.approle
+  role_name               = "${each.key}"
+  policies                = each.value["token_policies"]
+  token_no_default_policy = false
+  secret_id_num_uses      = lookup(each.value, "secret_id_num_uses", null)
+  secret_id_ttl           = lookup(each.value, "secret_id_ttl", null)
+  token_num_uses          = lookup(each.value, "token_num_uses", null)
+  token_ttl               = lookup(each.value, "token_ttl", null)
+  token_max_ttl           = lookup(each.value, "token_max_ttl", null)
 }
 
 resource "vault_approle_auth_backend_role_secret_id" "id" {
