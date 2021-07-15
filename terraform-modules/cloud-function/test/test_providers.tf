077f6feaf706817e6f1190790beeb8ce12f03c76
Jack Warren <jack@jackwarren.info>
2021-07-15 16:57:37 -0400

    fix validation

--- terraform-modules/cloud-function/test/test_providers.tf
+++ terraform-modules/cloud-function/test/test_providers.tf
@@ -1,8 +1,13 @@
+# Does not need auth for validation
 provider "google" {
   project = "broad-dsde-dev"
   region  = "us-central1"
 }
 
+# Expects VAULT_ADDR in environment, does not need auth for validation
+#   (Very old Terraform seems to be finnicky with reading an address
+#   provided literally to the provider during validation--this is only
+#   a quirk for running validation in this repo for old versions of
+#   Terraform and is not an issue for normal use cases)
 provider "vault" {
-  address = "https://clotho.broadinstitute.org:8200/"
 }
