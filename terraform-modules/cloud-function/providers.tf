97564b8d778e986d52c231effc664c7c392c9021
Jack Warren <jack@jackwarren.info>
2021-07-15 16:41:30 -0400

    older syntax for providers

--- terraform-modules/cloud-function/providers.tf
+++ terraform-modules/cloud-function/providers.tf
@@ -1,13 +1,7 @@
 terraform {
   required_version = ">= 0.12.19"
   required_providers {
-    google = {
-      source  = "hashicorp/google"
-      version = ">= 3.65.0"
-    }
-    vault = {
-      source  = "hashicorp/vault"
-      version = ">= 2.8.0"
-    }
+    google = ">= 3.65.0"
+    vault = ">= 2.8.0"
   }
 }
