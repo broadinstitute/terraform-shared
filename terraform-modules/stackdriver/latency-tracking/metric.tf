a20f38b45b7346d247681b030bfc9e7f330a0052
Jack Warren <jack@jackwarren.info>
2021-10-07 17:22:46 -0400

    formatting

--- terraform-modules/stackdriver/latency-tracking/metric.tf
+++ terraform-modules/stackdriver/latency-tracking/metric.tf
@@ -1,19 +1,19 @@
 resource "google_logging_metric" "latency_metric" {
   for_each = local.final_computed_endpoints
 
-  project = var.google_project
-  name = "${var.service}-${var.environment}-${var.environment}-${each.key}-endpoint-latency"
+  project     = var.google_project
+  name        = "${var.service}-${var.environment}-${var.environment}-${each.key}-endpoint-latency"
   description = "Latency of ${var.service}'s ${each.key} endpoint in ${var.environment}"
-  filter = <<-EOT
+  filter      = <<-EOT
     resource.type="http_load_balancer" AND
     resource.labels.project_id="${var.google_project}" AND
     httpRequest.requestUrl=~"${each.value.computed_regex}"
   EOT
 
   metric_descriptor {
-    metric_kind = "DELTA"
-    value_type = "DISTRIBUTION"
-    unit = "s"
+    metric_kind  = "DELTA"
+    value_type   = "DISTRIBUTION"
+    unit         = "s"
     display_name = "Latency for ${var.service} ${each.key} in ${var.environment}"
   }
 
@@ -23,8 +23,8 @@ resource "google_logging_metric" "latency_metric" {
   bucket_options {
     exponential_buckets {
       num_finite_buckets = 64
-      growth_factor = 2
-      scale = 0.01
+      growth_factor      = 2
+      scale              = 0.01
     }
   }
 }
