f62a2864a6916b55f8fa21baa4e723d4e731c40f
mflinn-broad <mflinn@broadinstitute.org>
2022-02-25 14:02:46 -0500

    match tf version used in gotc-deploy

--- terraform-modules/wfl-instance/versions.tf
+++ terraform-modules/wfl-instance/versions.tf
@@ -1,17 +1,17 @@
 terraform {
-  required_version = ">= 0.13.6"
+  required_version = ">= 0.12.25"
 
   required_providers {
     google = {
-      source = "hashicorp/google"
+      source  = "hashicorp/google"
       version = ">= 3.55.0"
     }
     google-beta = {
-      source = "hashicorp/google-beta"
+      source  = "hashicorp/google-beta"
       version = ">= 3.55.0"
     }
     random = {
-      source = "hashicorp/random"
+      source  = "hashicorp/random"
       version = ">= 3.0.1"
     }
   }
