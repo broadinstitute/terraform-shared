1325469f70661625d7d66369c350045318237cbe
David An <davidan@broadinstitute.org>
2022-06-07 16:00:12 -0400

    Quoted references are deprecated

--- terraform-modules/google-project/api-services.tf
+++ terraform-modules/google-project/api-services.tf
@@ -1,7 +1,7 @@
 module "enable-services" {
   source      = "github.com/broadinstitute/terraform-shared.git//terraform-modules/api-services?ref=services-0.3.0-tf-0.12"
   providers = {
-    google.target = "google.target"
+    google.target = google.target
   }
   project     = "${google_project.project.name}"
   services    = var.apis_to_enable
