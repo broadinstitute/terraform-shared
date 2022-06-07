305bc3a4d2d55a98b3a7a9772503817db32c6073
David An <davidan@broadinstitute.org>
2022-06-07 16:01:30 -0400

    Quoted references are deprecated

--- terraform-modules/google-project/project.tf
+++ terraform-modules/google-project/project.tf
@@ -1,5 +1,5 @@
 resource "google_project" "project" {
-  provider = "google.target"
+  provider = google.target
   name                  = "${var.project_name}"
   project_id            = "${var.project_name}"
   billing_account       = "${var.billing_account_id}"
