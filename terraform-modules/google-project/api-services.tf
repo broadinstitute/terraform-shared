5196c1694903e32d143ebaa3770b21b3ea7d35e0
David An <davidan@broadinstitute.org>
2022-06-10 10:06:12 -0400

    use implicit project

--- terraform-modules/google-project/api-services.tf
+++ terraform-modules/google-project/api-services.tf
@@ -1,5 +1,4 @@
 module "enable-services" {
   source      = "github.com/broadinstitute/terraform-shared.git//terraform-modules/api-services?ref=services-1.0.0"
-  project     = google_project.project.name
   services    = var.apis_to_enable
 }
