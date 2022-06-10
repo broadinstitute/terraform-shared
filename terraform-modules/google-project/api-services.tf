4c8dd89835cb2a328cb6845e5e8a5a388923ec2d
David An <davidan@broadinstitute.org>
2022-06-10 09:09:17 -0400

    api-services uses services-1.0.0

--- terraform-modules/google-project/api-services.tf
+++ terraform-modules/google-project/api-services.tf
@@ -1,5 +1,5 @@
 module "enable-services" {
-  source      = "github.com/broadinstitute/terraform-shared.git//terraform-modules/api-services?ref=services-0.3.0-tf-0.12"
+  source      = "github.com/broadinstitute/terraform-shared.git//terraform-modules/api-services?ref=services-1.0.0"
   providers = {
     google.target = google.target
   }
