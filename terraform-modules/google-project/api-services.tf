a4482b1d046bea590c27fc7c611e33c14b86e3d2
David An <davidan@broadinstitute.org>
2022-06-10 09:26:52 -0400

    implicit provider for google-project api-services

--- terraform-modules/google-project/api-services.tf
+++ terraform-modules/google-project/api-services.tf
@@ -1,8 +1,5 @@
 module "enable-services" {
   source      = "github.com/broadinstitute/terraform-shared.git//terraform-modules/api-services?ref=services-1.0.0"
-  providers = {
-    google.target = google.target
-  }
   project     = google_project.project.name
   services    = var.apis_to_enable
 }
