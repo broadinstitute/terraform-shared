6742575f66a22fc9e86a0feafd0fc265bd89229d
Henry Ferrara <ferrara@broadinstitute.org>
2022-07-21 14:11:18 -0400

    try map

--- terraform-modules/api-services/outputs.tf
+++ terraform-modules/api-services/outputs.tf
@@ -2,3 +2,8 @@
 output "services" {
   value = google_project_service.required-services
 }
+
+output "service_map" {
+  type  = map
+  value = { for indx,val in google_project_service.required-services: val[service] => val }
+}
