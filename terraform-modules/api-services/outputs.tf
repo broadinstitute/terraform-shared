d3b73357245bd277a3b63c255fa1032311cd1a17
Henry Ferrara <ferrara@broadinstitute.org>
2022-07-21 14:19:41 -0400

    output map

--- terraform-modules/api-services/outputs.tf
+++ terraform-modules/api-services/outputs.tf
@@ -1,8 +1,4 @@
 
 output "services" {
-  value = google_project_service.required-services
-}
-
-output "service_map" {
   value = { for indx,val in google_project_service.required-services: val.service => val }
 }
