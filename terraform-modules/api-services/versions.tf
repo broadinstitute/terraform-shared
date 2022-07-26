282fc3f3a530250bfc14d4c5754da63266d46dca
Henry Ferrara <ferrara@broadinstitute.org>
2022-07-26 16:14:19 -0400

    update versions.tf

--- terraform-modules/api-services/versions.tf
+++ terraform-modules/api-services/versions.tf
@@ -1,3 +1,12 @@
 terraform {
   required_version = ">= 0.12"
+
+  required_providers {
+    google = {
+      source = "hashicorp/google"
+    }
+    google-beta = {
+      source = "hashicorp/google-beta"
+    }
+  }
 }
