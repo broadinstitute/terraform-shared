bf58e671af2c03e19ea1017cd79cec38b76a6e09
mflinn-broad <mflinn@broadinstitute.org>
2024-01-12 12:04:30 -0500

    only bump to v4 provider

--- terraform-modules/k8s-node-pool/versions.tf
+++ terraform-modules/k8s-node-pool/versions.tf
@@ -4,11 +4,11 @@ terraform {
   required_providers {
     google = {
       source = "hashicorp/google"
-      version = ">= 5.0"
+      version = ">= 4.0"
     }
     google-beta = {
       source = "hashicorp/google-beta"
-      version = ">= 5.0"
+      version = ">= 4.0"
     }
   }
 }
