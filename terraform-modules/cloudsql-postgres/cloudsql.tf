d9fb8a9b2f52a0919717c8b9afea71ba70bbf072
Chelsea Hoover <chelsea@broadinstitute.org>
2025-06-27 12:24:22 -0400

    fix

--- terraform-modules/cloudsql-postgres/cloudsql.tf
+++ terraform-modules/cloudsql-postgres/cloudsql.tf
@@ -55,6 +55,7 @@ resource "google_sql_database_instance" "cloudsql_instance" {
     ip_configuration {
       ipv4_enabled    = (var.private_enable && !var.private_enable_public_ip) ? false : true
       private_network = var.private_enable == true ? local.private_network : null
+      enable_private_path_for_google_cloud_services = var.private_enable ? true : null
       require_ssl     = true
       dynamic "authorized_networks" {
         for_each = var.cloudsql_authorized_networks
